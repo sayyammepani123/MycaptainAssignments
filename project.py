480e0a156845f231537c32a229a93e24c1c3c1df
sayyam mepani <sayyammepani123@gmail.com>
2022-06-29 22:57:04 +0530

    initial commit

--- project.py
+++ project.py
@@ -1,3 +1,11 @@
 radius = float(input("radius of circle:"))
 print("area of circle is:", radius*radius*2.14)
-hello world
+
+import os.path
+
+split_tub = os.path.splitext('sayyam.py')
+
+file_name = split_tub[0]
+file_extention = split_tub[1]
+print("the file name is:",file_name)
+print("the file extention is:", file_extention)
